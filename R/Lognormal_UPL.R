5bc714862d9db45d41af8a841336d1ab18d7ef28
LuddaLudwig <sml2278@columbia.edu>
2025-09-19 13:52:14 -0400

    Lognormal UPL function

---
+++ R/Lognormal_UPL.R
@@ -0,0 +1,26 @@
+#' Calculate UPL assuming lognormally distributed emissions data
+#' @param dataset Emissions data from either the best source or top performers, must have a column named 'emissions'
+#' @param future_tests Integer of future runs to use in prediction, the default is 3 since compliance uses 1 test average of 3 runs.
+#' @param significance Level of significance from 0 to 1, the default is 0.99.
+#' @returns upper predictive limit at significance level for the average of the number of future test runs
+#' @export
+#' @references "An upper prediction limit for the arithmetic mean of a lognormal random variable" authored by Dulal Kumar Bhaumik and Robert David Gibbons 2004
+Lognormal_UPL=function(dataset,future_tests=3,significance=0.99){
+  dataset$ln_emiss=log(dataset$emissions)
+  dataset$ln_emiss=replace(dataset$ln_emiss,
+                           !is.finite(dataset$ln_emiss),NA)
+  n=length(dataset$emissions)
+  mean_log=mean(dataset$ln_emiss,na.rm=TRUE)
+  sd_log=sd(dataset$ln_emiss,na.rm=TRUE)
+  # using Gram-Charlmier Series A distribution approximation
+  beta2=(exp(4*sd_log^2)+2*exp(3*sd_log^2)+3*exp(2*sd_log^2)-3)/(future_tests*(exp(sd_log^2)-1)^2)+3*(1-1/future_tests)
+  beta1=sqrt(exp(sd_log^2)-1)*(exp(sd_log^2)+2)/sqrt(future_tests)
+  zvals=seq(-5,5,by=0.0101)
+  Fgzs=abs((1-(beta1/6)*(3*zvals-zvals^3)+(beta2-3)*(3-6*zvals^2+zvals^4)/24)*dnorm(zvals))
+  Fg_cdf=cumsum(Fgzs/sum(Fgzs))
+  zscore1=zvals[Fg_cdf>significance][1]
+  mean_convert=exp(mean_log+(sd_log^2)/2)
+  part2=sqrt(future_tests*exp(2*mean_log+sd_log^2)*(exp(sd_log^2)-1)+future_tests^2*exp(2*mean_log+sd_log^2)*(((sd_log^2)/n)+(sd_log^4)/(2*(n-1))))
+  PI99_lnorm=mean_convert+(zscore1/future_tests)*part2
+  return(PI99_lnorm)
+}
---
+++ tests/testthat/test-Lognormal_UPL.R
@@ -0,0 +1,21 @@
+test_that("Lognormal_UPL() calculates UPL assuming emissions data are normal", {
+  top5=tibble(emissions=c(1,2,1.5,
+                          1.2,3,2.2,
+                          0.2,0.4,0.7,
+                          0.1,0.2,0.3,1.1,1.2,1.3,
+                          0.1,0.1,0.15,0.16,0.19,1.8),
+              sources=c(rep('A',3),
+                        rep('B',3),
+                        rep('C',3),
+                        rep('D',6),
+                        rep('E',6)))
+  dat_topmeans=top5%>%group_by(sources)%>%summarize(means=mean(emissions),
+                                                    counts=n())
+  dat_topmeans$sources=as.factor(dat_topmeans$sources)
+  dat_topmeans$sources=fct_reorder(dat_topmeans$sources,
+                                   dat_topmeans$means,.desc = FALSE)
+  top5$sources=factor(top5$sources,levels=levels(dat_topmeans$sources))
+  top5=arrange(top5,sources)
+  test_result=Lognormal_UPL(dataset=top5)
+  expect_equal(test_result,4.80939868)
+})
