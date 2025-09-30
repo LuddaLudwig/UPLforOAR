473403337643a8ec51967a612d6ed6a0a23e734c
LuddaLudwig <sml2278@columbia.edu>
2025-09-30 14:26:20 -0400

    fix name of run_mcmc

--- tests/testthat/test-run_likelihood.R
+++ tests/testthat/test-run_likelihood.R
@@ -27,7 +27,7 @@ test_that("run_likelihood() runs JAGS models from setup_likelihood()", {
   run_mcmc=as.matrix(runmod$run_results$mcmc[[1]])
   # saveRDS(run_mcmc,'test_mcmc.rds')
   load_results=readRDS(test_path('test_run','test_mcmc.rds'))
-  expect_equal(run_results,load_results)
+  expect_equal(run_mcmc,load_results)
   expect_equal(dim(run_results$mcmc[[1]]),c(10000,length(xvals)+runcount+nrow(top5)))
   expect_equal(run_results$burnin,20000)
   expect_equal(run_results$monitor,c("emission_hat",'pdf_obs','pdf_hat'))
