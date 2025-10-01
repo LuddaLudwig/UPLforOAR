b45f53a8a27bcfeaee4dc00f1b0c9331ec7062de
LuddaLudwig <sml2278@columbia.edu>
2025-10-01 13:49:48 -0400

    fix function name to match

--- R/multi_source_theme.R
+++ R/multi_source_theme.R
@@ -3,7 +3,7 @@
 #' This function sets ggplot2 theme elements
 #' @importFrom ggplot2 %+replace%
 #' @export
-pop_distr_theme <- function () {
+multi_source_theme <- function () {
   multi_source_theme=ggplot2::theme_bw() %+replace%
   ggplot2::theme(plot.margin=grid::unit(c(0.1,0.5,0.5,0.1),"cm"),
                          legend.direction = 'horizontal',
