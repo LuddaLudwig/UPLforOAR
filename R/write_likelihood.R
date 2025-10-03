8ccc1f6aca6913b4e64c906869b6c196b21fe0dd
LuddaLudwig <sml2278@columbia.edu>
2025-10-03 09:14:33 -0400

    u_ln can be negative

--- R/write_likelihood.R
+++ R/write_likelihood.R
@@ -41,7 +41,7 @@ write_likelihood=function(distribution,write_wd=NULL){
     cat("model {
       # priors
           sd_ln ~ dunif( 0.001*sdOfLogY , 1000*sdOfLogY )
-          u_ln ~ dnorm( meanOfLogY , 0.001*1/sdOfLogY^2 )T(0,)
+          u_ln ~ dnorm( meanOfLogY , 0.001*1/sdOfLogY^2 ) #this is in log-space, so it can be negative
           tau_ln<-1/(sd_ln^2)
 
       #likelihood
