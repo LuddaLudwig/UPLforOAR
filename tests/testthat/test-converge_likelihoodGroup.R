fd02e5e71998770d0d9af6db77f9030e60f655f5
LuddaLudwig <sml2278@columbia.edu>
2025-11-21 17:51:38 -0500

    fix test

--- tests/testthat/test-converge_likelihoodGroup.R
+++ tests/testthat/test-converge_likelihoodGroup.R
@@ -29,19 +29,8 @@ test_that("converge_likelihoodGroup() runs gelman diagnostics for convergence te
   runmod=run_likelihoodGroup(model_input=JAGS_model_stuff,
                              xvals=xvals,future_runs=runcount)
   conv_results=converge_likelihoodGroup(runmod)
-  expect_equal(round(conv_results$gelman_diag,3),c(1.001, 1.004, 1.004, 1.022,
-                                                   1.002, 1.002, 1.000, 1.001,
-                                                   1.002, 1.002, 1.003, 1.001,
-                                                   1.002, 1.003))
-  expect_equal(conv_results$params,c('pop_rate_mu', 'pop_shape_mu',
-                                     'pop_rate_sd', 'pop_shape_sd',
-                                     "group_rate_B", "group_rate_C",
-                                     "group_rate_A",  "group_rate_E",
-                                     "group_rate_D", "group_shape_B",
-                                     "group_shape_C", "group_shape_A",
-                                     "group_shape_E", "group_shape_D"))
-  expect_equal(conv_results$convYN,c('Yes','Yes','Yes','Yes','Yes','Yes','Yes',
-                                     'Yes','Yes','Yes','Yes','Yes','Yes','Yes'))
-
+  # saveRDS(conv_results,test_path('test_conv','test-conv_results.rds'))
+  load_results=readRDS(test_path('test_conv','test-conv_results.rds'))
+  expect_equal(load_results, conv_results)
 })
 
